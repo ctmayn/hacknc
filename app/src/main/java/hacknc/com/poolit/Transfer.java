64cd3eb64315c1556cd7d8cd5a17c9c5be350cbb
ctmayn2 <ctmayn2@gmail.com>
2016-10-29 17:16:11 -0400

    finished the transfer class

--- app/src/main/java/hacknc/com/poolit/Transfer.java
+++ app/src/main/java/hacknc/com/poolit/Transfer.java
@@ -1,15 +1,82 @@
 package hacknc.com.poolit;
 
+import com.reimaginebanking.api.nessieandroidsdk.*;
+import com.reimaginebanking.api.nessieandroidsdk.constants.AccountType;
+import com.reimaginebanking.api.nessieandroidsdk.models.Account;
+import com.reimaginebanking.api.nessieandroidsdk.models.PostResponse;
+import com.reimaginebanking.api.nessieandroidsdk.models.PutDeleteResponse;
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.AccountClient;
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+import com.reimaginebanking.api.nessieandroidsdk.requestservices.AccountService;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
 /**
  * Created by Christopher on 10/29/2016.
  */
 
 public class Transfer {
-    double value;
+    int value;
     User sender;
     User recipient;
+    private static final String KEY = "d1cab498a02db5a8370dd4fe90e52321"
 
-    public Transfer(User sender, User recipeint, double value){
-        
+    public Transfer(User sender, User recipeint, final int value){
+        NessieClient client = NessieClient.getInstance(KEY);
+        client.ACCOUNT.getAccount(sender.getAccountID(), new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                Account senderAcc = (Account) result;
+                NessieClient client = NessieClient.getInstance(KEY);
+                Account newAcc = new Account.Builder()
+                        .balance(senderAcc.getBalance() - value)
+                        .build();
+                client.ACCOUNT.updateAccount(senderAcc.getId(), newAcc, new NessieResultsListener() {
+                    @Override
+                    public void onSuccess(Object result) {
+
+                    }
+
+                    @Override
+                    public void onFailure(NessieError error) {
+
+                    }
+                });
+            }
+            @Override
+            public void onFailure(NessieError error) {
+                throw new IllegalArgumentException("Account ID did not match any in the system!");
+            }
+        });
+        client.ACCOUNT.getAccount(sender.getAccountID(), new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                Account recipientAcc = (Account) result;
+                NessieClient client = NessieClient.getInstance(KEY);
+                Account newAcc = new Account.Builder()
+                        .balance(recipientAcc.getBalance() - value)
+                        .build();
+                client.ACCOUNT.updateAccount(recipientAcc.getId(), newAcc, new NessieResultsListener() {
+                    @Override
+                    public void onSuccess(Object result) {
+
+                    }
+
+                    @Override
+                    public void onFailure(NessieError error) {
+
+                    }
+                });
+            }
+            @Override
+            public void onFailure(NessieError error) {
+                throw new IllegalArgumentException("Account ID did not match any in the system!");
+            }
+        });
     }
 }
