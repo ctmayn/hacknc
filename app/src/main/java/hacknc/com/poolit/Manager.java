4a3465c7ad2b7d5488aea987f7d2e4556e1a9209
Celedric <robbyfseligson@gmail.com>
2016-10-29 22:41:19 -0400

    Added manager commends & logout.

--- app/src/main/java/hacknc/com/poolit/Manager.java
+++ app/src/main/java/hacknc/com/poolit/Manager.java
@@ -9,8 +9,17 @@ import java.util.List;
  */
 
 public class Manager {
+    /**
+     * The user who is currently logged in.
+     */
     private User currentUser;
 
+    /**
+     * Logs the current user in to the program.
+     * @param id The id of the user to be logged in.
+     * @param pass The password of the user to be logged in.
+     * @throws Exception
+     */
     public void login(String id, String pass) throws Exception {
         if(currentUser != null){
             throw new Exception("Already logged in.");
@@ -19,27 +28,60 @@ public class Manager {
         currentUser = Server.getInstance().login(id, hashed);
     }
 
+    /**
+     * Logs the current user out of the application.
+     */
+    public void logout(){
+        currentUser = null;
+    }
+
+    /**
+     * Returns a list of friends of the user.
+     * @return The list of friends.
+     */
     public List<User> friends(){
 
         return currentUser.getFriends();
 
     }
 
+    /**
+     * Finds a user given the user's name.
+     * @param name The name of the user to be found.
+     * @return The list of users found.
+     */
     public List<User> findUser(String name){
         return Server.getInstance().getUsersByName(name);
     }
 
+    /**
+     *
+     * @param friend The user to be added.
+     */
     public void addFriend(User friend){
         currentUser.addFriend(friend);
     }
+
+    /**
+     *
+     * @param event The event to be added.
+     */
     public void addEvent(Event event){
         Server.getInstance().addEvent(event);
     }
 
+    /**
+     *
+     * @param friend The user that will be removed.
+     */
     public void removeFriend(User friend){
         currentUser.removeFriend(friend);
     }
 
+    /**
+     *
+     * @return The events the user is a part of.
+     */
     public List<Event> userEvents(){
         return  currentUser.getEvents();
     }
